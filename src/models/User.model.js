921885f494760bd81bca897508002f6e1d000010
Hammarou <139810496+Hammarou@users.noreply.github.com>
2024-11-19 01:31:47 -0500

    Added middleware

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,16 +1,31 @@
 import mongoose from "mongoose";
 
-const { model, Schema, Types} = mongoose;
+const { model, Schema, Types } = mongoose;
 
 const UserSchema = new Schema({
-  'username': {type: String, unique: true, required: true},
-  'email': {type: String, unique: true, required: true},
-  'thoughts': {type: [Types.ObjectId]},
-  'friends': {type: [Types.ObjectId]},
+  username: { type: String, unique: true, required: true },
+  email: { type: String, unique: true, required: true },
+  thoughts: { type: [Types.ObjectId] },
+  friends: { type: [Types.ObjectId] },
 });
 
-UserSchema.virtual('friendCount').get(function(){
-  return this.friends.length
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
-export const User = model('User', UserSchema);
+// Middleware to check for existing username or email
+UserSchema.pre("save", async function (next) {
+  const user = this;
+  const existingUser = await User.findOne({
+    $or: [{ username: user.username }, { email: user.email }],
+  });
+
+  if (existingUser) {
+    const error = new Error("Username or Email already exists");
+    return next(error);
+  }
+  next();
+});
+
+export const User = model("User", UserSchema);
+User.createIndexes();
